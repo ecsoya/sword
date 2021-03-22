b9462f2dd216a20e8b03acf678ff063689aa1fd0
jin.liu@soyatec.com <jin.liu@soyatec.com>
2021-03-22 14:59:59 +0800

    删除重复的API

--- sword-module-api/sword-app-mining/src/main/java/com/soyatec/sword/mining/controller/UserReferralController.java
+++ sword-module-api/sword-app-mining/src/main/java/com/soyatec/sword/mining/controller/UserReferralController.java
@@ -9,16 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.soyatec.sword.common.annotation.RepeatSubmit;
 import com.soyatec.sword.common.core.controller.BaseController;
 import com.soyatec.sword.common.core.domain.CommonResult;
-import com.soyatec.sword.common.core.page.TableDataInfo;
 import com.soyatec.sword.user.domain.UserReferrer;
-import com.soyatec.sword.user.domain.UserReferrerTeam;
 import com.soyatec.sword.user.service.IUserReferrerService;
-import com.soyatec.sword.user.service.IUserReferrerTeamService;
 import com.soyatec.sword.utils.SwordUtils;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 
 /**
  * 用户推荐码接口
@@ -32,9 +28,6 @@ public class UserReferralController extends BaseController {
 
 	@Autowired
 	private IUserReferrerService referrerService;
-
-	@Autowired
-	private IUserReferrerTeamService referrerTeamService;
 
 	/**
 	 * 获取推荐码
@@ -55,21 +48,6 @@ public class UserReferralController extends BaseController {
 	public CommonResult<UserReferrer> refresh() {
 		Long userId = SwordUtils.getUserId();
 		return CommonResult.build(referrerService.refreshUserReferrerById(userId));
-	}
-
-	@ApiOperation(value = "获取直推团队人数", notes = "直推团队人数：\n1、total - 全部 \n2、today - 今日 \n3、yesterday - 昨日 \n4、单位为 人")
-	@GetMapping("/team")
-	public CommonResult<UserReferrerTeam> team() {
-		Long userId = SwordUtils.getUserId();
-		return CommonResult.build(referrerTeamService.selectUserReferrerTeamByUserId(userId));
-	}
-
-	@ApiOperation(value = "获取直推团队列表", notes = "此处只取直推团队")
-	@GetMapping("/list")
-	public TableDataInfo list(@ApiParam("当前页数，第几页") Integer pageNum, @ApiParam("每页显示多少条") Integer pageSize) {
-		startPage("create_time desc");
-		Long userId = SwordUtils.getUserId();
-		return getDataTable(referrerTeamService.selectUserReferrerTeamListByUserId(userId));
 	}
 
 }
--- sword-module-api/sword-app-mining/src/main/java/com/soyatec/sword/mining/controller/UserReferrerTeamController.java
+++ sword-module-api/sword-app-mining/src/main/java/com/soyatec/sword/mining/controller/UserReferrerTeamController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.soyatec.sword.common.core.controller.BaseController;
 import com.soyatec.sword.common.core.domain.CommonResult;
 import com.soyatec.sword.common.core.page.TableDataInfo;
-import com.soyatec.sword.mining.utils.SwordMiningUtils;
 import com.soyatec.sword.user.domain.UserReferrerTeam;
 import com.soyatec.sword.user.service.IUserReferrerTeamService;
+import com.soyatec.sword.utils.SwordUtils;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -29,18 +29,33 @@ public class UserReferrerTeamController extends BaseController {
 	@Autowired
 	private IUserReferrerTeamService userReferrerTeamService;
 
+//	@ApiOperation(value = "获取直推团队人数", notes = "直推团队人数：\n1、total - 全部 \n2、today - 今日 \n3、yesterday - 昨日 \n4、单位为 人")
+//	@GetMapping
+//	public CommonResult<UserReferrerTeam> referral() {
+//		Long userId = SwordMiningUtils.getUserId();
+//		return CommonResult.build(userReferrerTeamService.selectUserReferrerTeamByUserId(userId));
+//	}
+//
+//	@ApiOperation("获取直推团队列表（支持分页）")
+//	@GetMapping("/members")
+//	public TableDataInfo referralMembers(@ApiParam("当前页数，第几页") Integer pageNum, @ApiParam("每页显示多少条") Integer pageSize) {
+//		startPage();
+//		Long userId = SwordMiningUtils.getUserId();
+//		return getDataTable(userReferrerTeamService.selectUserReferrerTeamListByUserId(userId));
+//	}
+
 	@ApiOperation(value = "获取直推团队人数", notes = "直推团队人数：\n1、total - 全部 \n2、today - 今日 \n3、yesterday - 昨日 \n4、单位为 人")
-	@GetMapping
-	public CommonResult<UserReferrerTeam> referral() {
-		Long userId = SwordMiningUtils.getUserId();
+	@GetMapping()
+	public CommonResult<UserReferrerTeam> team() {
+		Long userId = SwordUtils.getUserId();
 		return CommonResult.build(userReferrerTeamService.selectUserReferrerTeamByUserId(userId));
 	}
 
-	@ApiOperation("获取直推团队列表（支持分页）")
-	@GetMapping("/members")
-	public TableDataInfo referralMembers(@ApiParam("当前页数，第几页") Integer pageNum, @ApiParam("每页显示多少条") Integer pageSize) {
-		startPage();
-		Long userId = SwordMiningUtils.getUserId();
+	@ApiOperation(value = "获取直推团队列表（支持分页）", notes = "此处只取直推团队")
+	@GetMapping("/list")
+	public TableDataInfo list(@ApiParam("当前页数，第几页") Integer pageNum, @ApiParam("每页显示多少条") Integer pageSize) {
+		startPage("create_time desc");
+		Long userId = SwordUtils.getUserId();
 		return getDataTable(userReferrerTeamService.selectUserReferrerTeamListByUserId(userId));
 	}
 
