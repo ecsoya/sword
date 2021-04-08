d1b2a41b4fd27349fcb50fe74b8b92eaf21c9f4e
AngryRED <angryred@qq.com>
2021-04-08 13:41:41 +0800

    更新默认SQL脚本

--- sword-module-deploy/sql/1.base.sql
+++ sword-module-deploy/sql/1.base.sql
@@ -108,7 +108,7 @@ LOCK TABLES `sys_config` WRITE;
 
 INSERT INTO `sys_config` (`config_id`, `config_name`, `config_key`, `config_value`, `config_type`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`)
 VALUES
-	(1,'主框架页-默认皮肤样式名称','sys.index.skinName','skin-blue','Y','admin','2021-01-05 10:29:53','',NULL,'蓝色 skin-blue、绿色 skin-green、紫色 skin-purple、红色 skin-red、黄色 skin-yellow'),
+	(1,'主框架页-默认皮肤样式名称','sys.index.skinName','skin-primary','Y','admin','2021-01-05 10:29:53','',NULL,'蓝色 skin-blue、绿色 skin-green、紫色 skin-purple、红色 skin-red、黄色 skin-yellow'),
 	(2,'用户管理-账号初始密码','sys.user.initPassword','123456','Y','admin','2021-01-05 10:29:53','',NULL,'初始化密码 123456'),
 	(3,'主框架页-侧边栏主题','sys.index.sideTheme','theme-dark','Y','admin','2021-01-05 10:29:53','',NULL,'深黑主题theme-dark，浅色主题theme-light，深蓝主题theme-blue'),
 	(4,'账号自助-是否开启用户注册功能','sys.account.registerUser','false','Y','admin','2021-01-05 10:29:53','',NULL,'是否开启注册用户功能（true开启，false关闭）'),
