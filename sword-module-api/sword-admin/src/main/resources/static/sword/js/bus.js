1b09fb69796dddaa82e1744c50df22ebeb647784
AngryRED <angryred@qq.com>
2021-04-13 09:57:16 +0800

    更新开放策略

--- sword-module-api/sword-admin/src/main/resources/static/sword/js/bus.js
+++ sword-module-api/sword-admin/src/main/resources/static/sword/js/bus.js
@@ -49,3 +49,66 @@ function verifyCode(callback) {
     			}
     		});
 }
+
+	var _EXPIRE_TIME = 5*60*1000;//5分钟没有操作，则关闭
+    var _interval_handler=-1;
+    
+    function enablePermissionFlag(title, key, callback) {
+    	if (getPermissionFlag(key) === '') {
+    		return;
+    	}
+    	if (notifyEnabled){
+	    	var label = title + '此功能需输入验证码验证后使用！(<small>验证码会在点击确认后发出，5分钟之内有效</small>)'
+	    	$.modal.confirm(label, function(){
+	    		verifyCode(function(){
+	    			doEnablePermissionFlag(key, callback);
+	    		});
+	    	})
+    	} else {
+			doEnablePermissionFlag(key, callback);
+		}
+    }
+function doEnablePermissionFlag(key, callback){
+	if (window.sessionStorage) {
+	    				$('#' + key).hide();
+						window.sessionStorage.setItem(key, true);
+						window.sessionStorage.setItem(key + "_expireTime", new Date().getTime());
+						_interval_handler=setInterval(function(){
+							checkExpired(key, callback);
+						}, 10*1000);
+					  	if (callback) {
+							callback();
+						}
+					}
+}
+function getPermissionFlag(key, value) {
+	if (window.sessionStorage) {
+		var storage = window.sessionStorage.getItem(key);
+	    var storeLastTime= window.sessionStorage.getItem(key+"_expireTime")?window.sessionStorage.getItem(key + "_expireTime"):-1;
+	    if (storeLastTime == -1 || (new Date()).getTime()-storeLastTime>_EXPIRE_TIME){
+   		    return 'hidden'; //过期
+		}
+		if ('true' == storage) {
+			return value;
+		}
+	}
+   	return 'hidden';
+}
+
+function checkExpired(key, callback) {
+    	if (!key || !window.sessionStorage) {
+    		return;
+   		}
+   	    var storeLastTime= window.sessionStorage.getItem(key + "_expireTime")?window.sessionStorage.getItem(key + "_expireTime"):-1;
+    	if (storeLastTime==-1) {
+    		clearInterval(_interval_handler);
+    	} else if ((new Date()).getTime()-storeLastTime>_EXPIRE_TIME) {  //过期了
+    		$('#' + key).show();
+    	    window.sessionStorage.setItem(key, false);
+    	    window.sessionStorage.removeItem(key + "_expireTime");
+    	    clearInterval(_interval_handler);
+    	    if (callback) {
+				callback();
+			}
+    	}
+	}
