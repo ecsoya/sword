31e876bc8d10f162cf9b9045542e53907828de9d
AngryRED <angryred@qq.com>
2021-04-16 16:58:45 +0800

    更新TickerCacheTask

--- sword-module-mining/sword-mining-app/src/main/java/com/soyatec/sword/mining/task/TickerCacheTask.java
+++ sword-module-mining/sword-mining-app/src/main/java/com/soyatec/sword/mining/task/TickerCacheTask.java
@@ -1,6 +1,7 @@
 package com.soyatec.sword.mining.task;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +11,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import com.soyatec.sword.common.utils.DateUtils;
+import com.soyatec.sword.common.utils.StringUtils;
+import com.soyatec.sword.constants.IMiningConstants;
 import com.soyatec.sword.exceptions.LockableException;
 import com.soyatec.sword.mining.service.IMiningSymbolService;
 import com.soyatec.sword.service.ILockService;
@@ -33,12 +36,13 @@ public class TickerCacheTask {
 	private ILockService lockService;
 
 	@Scheduled(cron = "0 */5 * * * ?")
-	public void cacheKcPrice() {
+	public void cacheTicker() {
 		log.info("TickerCacheTask at {}", DateUtils.getNowDateStr());
 		boolean locked = false;
 		try {
 			locked = lockService.tryLock(LOCK_NAME);
-			final List<String> symbols = symbolService.selectMiningSymbols();
+			final List<String> symbols = symbolService.selectMiningSymbols().stream()
+					.filter(s -> !StringUtils.equals(IMiningConstants.SYMBOL_USDT, s)).collect(Collectors.toList());
 			symbols.forEach(symbol -> walletService.updateTickerCache(symbol));
 		} catch (final LockableException e) {
 			log.warn("TickerCacheTask missing");
