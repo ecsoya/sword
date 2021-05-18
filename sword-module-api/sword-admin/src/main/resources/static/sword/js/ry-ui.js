d2f29791d11f15a4ee360bb75135ebf65aa2a556
amlihao <amlihao@DESKTOP-LHA>
2021-05-18 20:17:30 +0800

    拖拽上传插件及添加弹窗

--- sword-module-api/sword-admin/src/main/resources/static/sword/js/ry-ui.js
+++ sword-module-api/sword-admin/src/main/resources/static/sword/js/ry-ui.js
@@ -828,6 +828,46 @@ var table = {
             	    }
             	});
             },
+            openNoOk: function (title, url, width, height, callback) {
+                //如果是移动端，就使用自适应大小弹窗
+                if (navigator.userAgent.match(/(iPhone|iPod|Android|ios)/i)) {
+                    width = 'auto';
+                    height = 'auto';
+                }
+                if ($.common.isEmpty(title)) {
+                    title = false;
+                }
+                if ($.common.isEmpty(url)) {
+                    url = "/404.html";
+                }
+                if ($.common.isEmpty(width)) {
+                    width = 800;
+                }
+                if ($.common.isEmpty(height)) {
+                    height = ($(window).height() - 50);
+                }
+                if ($.common.isEmpty(callback)) {
+                    callback = function (index, layero) {
+                        var iframeWin = layero.find('iframe')[0];
+                        iframeWin.contentWindow.submitHandler();
+                    }
+                }
+                var lay = layer.open({
+                    type: 2,
+                    area: [width + 'px', height + 'px'],
+                    fix: false,
+                    maxmin: true,
+                    shade: 0.3,
+                    title: title,
+                    content: url,
+                    btn: ['关闭'],
+                    shadeClose: true,
+                    cancel: function (index) {
+                		return true;
+                    }
+                });
+                return lay;
+            },
             // 弹出层指定参数选项
             openOptions: function (options) {
             	var _url = $.common.isEmpty(options.url) ? "/404.html" : options.url; 
